3c6ede6a686336ecfb0cc2cc39639f63ebe2d6e3
Sarah Holder <sr.holder@yahoo.com>
2020-05-15 17:51:51 -0500

    basket working with poisonous and deadly mushroom working

--- src/App/App.js
+++ src/App/App.js
@@ -3,23 +3,37 @@ import './App.scss';
 
 import mushroomData from '../helpers/data/mushroomData';
 import Forest from '../components/Forest';
+import Basket from '../components/Basket';
 
 class App extends React.Component {
   state = {
     mushrooms: [],
+    basket: [],
   }
 
   componentDidMount() {
     const mushrooms = mushroomData.getMushrooms();
     this.setState({ mushrooms });
+
+    const basket = mushroomData.getBasket();
+    this.setState({ basket });
+  }
+
+  pickAMushroomEvent = (e) => {
+    mushroomData.pickAMushroom();
+    const basket = mushroomData.getBasket();
+    this.setState({ basket });
   }
 
   render() {
     const { mushrooms } = this.state;
+    const { basket } = this.state;
 
     return (
       <div className="App">
         <h1>MUSHROOM MANIA</h1>
+        <button className="btn btn-danger" onClick={this.pickAMushroomEvent}>PICK MUSHROOM</button>
+        <Basket basket={basket}/>
         <Forest mushrooms={mushrooms}/>
       </div>
     );
--- src/components/Forest.js
+++ src/components/Forest.js
@@ -1,18 +1,21 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import mushroomShape from '../helpers/propz/mushroomShape';
-
+import PropTypes from 'prop-types';
+import mushroomShape from '../helpers/propz/mushroomShape';
 import Mushroom from './Mushroom';
 
 class Forest extends React.Component {
+  static propTypes = {
+    mushrooms: PropTypes.arrayOf(mushroomShape.mushroomShape),
+  }
+
   render() {
     const { mushrooms } = this.props;
-    const makeMushrooms = mushrooms.map((mushroom) => (
+    const buildForest = mushrooms.map((mushroom) => (
       <Mushroom key={mushroom.id} mushroom={mushroom}/>
     ));
     return (
       <div className="Forest d-flex flex-wrap">
-        {makeMushrooms}
+        {buildForest}
       </div>
     );
   }
